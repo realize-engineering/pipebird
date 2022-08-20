2e91d948f9023aded63e921b14107317172f3724
Ian Edwards <ian@realizefi.com>
2022-08-20 18:08:38 -0400

    fix: expire in a day by default

--- lib/aws/signer.ts
+++ lib/aws/signer.ts
@@ -5,15 +5,20 @@ import { S3 } from "./s3.js";
 import { env } from "../env.js";
 
 const BUCKET = env.PROVISIONED_BUCKET_NAME;
+const DAY_IN_SECONDS = 86400;
 
-export const getPresignedURL = async (key: string, bucket: string = BUCKET) => {
+export const getPresignedURL = async (
+  key: string,
+  bucket: string = BUCKET,
+  expiresIn: number = DAY_IN_SECONDS,
+) => {
   const command = new GetObjectCommand({
     Bucket: bucket,
     Key: key,
   });
 
   const signedUrl = await getSignedUrl(S3, command, {
-    expiresIn: 3600,
+    expiresIn,
   });
 
   return signedUrl;
