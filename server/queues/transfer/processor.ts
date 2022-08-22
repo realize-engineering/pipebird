8d5365174b4d3e5608f11bee44cd504576a7a415
Timothy Goltser <tim@realizefi.com>
2022-08-22 13:51:44 -0400

    Add transfer status updates to transfer worker

--- server/queues/transfer/processor.ts
+++ server/queues/transfer/processor.ts
@@ -5,62 +5,89 @@ import { logger } from "../../../lib/logger.js";
 import dfd from "danfojs-node";
 import { uploadObject } from "../../../lib/aws/upload.js";
 import { TransferQueueJobData } from "./scheduler.js";
+import { db } from "../../../lib/db.js";
 
 export default async function (job: Job<TransferQueueJobData>) {
-  logger.info("Processor is handling job with transfer:", job.data);
+  try {
+    await db.transfer.update({
+      where: {
+        id: job.data.id,
+      },
+      data: {
+        status: "PENDING",
+      },
+    });
 
-  const transfer = job.data;
-  const destination = transfer.destination;
-  const configuration = destination.configuration;
+    logger.info("Processor is handling job with transfer:", job.data);
 
-  if (!configuration) {
-    logger.error(
-      `No configuration found for transfer with ID ${transfer.id}, aborting`,
-    );
+    const transfer = job.data;
+    const destination = transfer.destination;
+    const configuration = destination.configuration;
 
-    return "false";
-  }
-
-  const view = configuration.view;
-  const source = view.source;
-
-  const {
-    sourceType: dbType,
-    host,
-    port,
-    username,
-    password,
-    name: dbName,
-  } = source;
-
-  const conn = await getConnection({
-    dbType,
-    host,
-    port,
-    username,
-    password,
-    dbName,
-  });
-
-  if (conn.status !== "REACHABLE") {
-    logger.error(
-      `Source with ID ${source.id} is unreachable, aborting transfer ${transfer.id}`,
-    );
-    return "false";
-  }
-
-  const rows = await conn.connection.query(view.tableExpression, {
-    type: QueryTypes.SELECT,
-  });
-
-  const df = new dfd.DataFrame(rows);
-
-  switch (destination.destinationType) {
-    case "PROVISIONED_S3": {
-      await uploadObject(Buffer.from(df.toCSV()));
-      break;
+    if (!configuration) {
+      throw new Error(
+        `No configuration found for transfer with ID ${transfer.id}, aborting`,
+      );
     }
-  }
 
-  return "true";
+    const view = configuration.view;
+    const source = view.source;
+
+    const {
+      sourceType: dbType,
+      host,
+      port,
+      username,
+      password,
+      name: dbName,
+    } = source;
+
+    const conn = await getConnection({
+      dbType,
+      host,
+      port,
+      username,
+      password,
+      dbName,
+    });
+
+    if (conn.status !== "REACHABLE") {
+      throw new Error(
+        `Source with ID ${source.id} is unreachable, aborting transfer ${transfer.id}`,
+      );
+    }
+
+    const rows = await conn.connection.query(view.tableExpression, {
+      type: QueryTypes.SELECT,
+    });
+
+    const df = new dfd.DataFrame(rows);
+
+    switch (destination.destinationType) {
+      case "PROVISIONED_S3": {
+        await uploadObject(Buffer.from(df.toCSV()));
+        break;
+      }
+    }
+
+    await db.transfer.update({
+      where: {
+        id: transfer.id,
+      },
+      data: {
+        status: "COMPLETE",
+      },
+    });
+  } catch (error) {
+    logger.error(error);
+
+    await db.transfer.update({
+      where: {
+        id: job.data.id,
+      },
+      data: {
+        status: "FAILED",
+      },
+    });
+  }
 }
