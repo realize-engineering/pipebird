3048d35cf6a9f71278aa7eff1516eea94c270080
Timothy Goltser <tim@realizefi.com>
2022-08-28 15:34:02 -0400

    Add GZIP compression on CSV stream

--- server/queues/transfer/processor.ts
+++ server/queues/transfer/processor.ts
@@ -11,6 +11,7 @@ import {
   getUniqueTableName,
   removeLoadedData,
 } from "../../../lib/snowflake/load.js";
+import zlib from "node:zlib";
 
 const buildTemplatedQuery = ({
   viewColumns,
@@ -160,22 +161,26 @@ export default async function (job: Job<TransferQueueJobData>) {
       );
     }
 
-    const queryDataStream = sourceConnection.extractToCsvUnsafe(
-      buildTemplatedQuery({
-        viewColumns: view.columns.map((col) => col.name),
-        resultColumns: configuration.columns,
-        tableName: view.tableName,
-        tenantColumn: view.columns.filter((col) => col.isTenantColumn)[0].name,
-        tenantId: destination.tenantId,
-        lastModifiedColumn: view.columns.filter((col) => col.isLastModified)[0]
-          .name,
-        lastModifiedAt: destination.lastModifiedAt.toISOString(),
-      }),
-    );
+    const queryDataStream = sourceConnection
+      .extractToCsvUnsafe(
+        buildTemplatedQuery({
+          viewColumns: view.columns.map((col) => col.name),
+          resultColumns: configuration.columns,
+          tableName: view.tableName,
+          tenantColumn: view.columns.filter((col) => col.isTenantColumn)[0]
+            .name,
+          tenantId: destination.tenantId,
+          lastModifiedColumn: view.columns.filter(
+            (col) => col.isLastModified,
+          )[0].name,
+          lastModifiedAt: destination.lastModifiedAt.toISOString(),
+        }),
+      )
+      .pipe(zlib.createGzip());
 
     switch (destination.destinationType) {
       case "PROVISIONED_S3": {
-        await uploadObject(queryDataStream);
+        await uploadObject({ contents: queryDataStream, extension: "gz" });
         break;
       }
 
@@ -208,7 +213,11 @@ export default async function (job: Job<TransferQueueJobData>) {
         }
 
         const pathPrefix = `snowflake/${destination.tenantId}/${destination.id}`;
-        await uploadObject(queryDataStream, pathPrefix);
+        await uploadObject({
+          contents: queryDataStream,
+          pathPrefix,
+          extension: "gz",
+        });
 
         const tempStageName = `SharedData_TempTable_${
           destination.id
