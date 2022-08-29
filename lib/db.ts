c5fc9bbbf29a89c356c14f96e2015e16aa13d950
Timothy Goltser <tim@realizefi.com>
2022-08-29 18:34:16 -0400

    Add identifier quoting util, remove pg-copy-streams in favor of app-level stream writer

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,28 @@
 import { PrismaClient } from "@prisma/client";
+import { raw, Sql } from "sql-template-tag";
 
 export const db = new PrismaClient();
+
+export const quoteIdentifier = (value: string): Sql =>
+  raw(
+    `"${Array.from(value).reduce(
+      (identifier, char) =>
+        char === '"' ? identifier + '""' : identifier + char,
+      "",
+    )}"`,
+  );
+
+export const quoteIdentifiers = (
+  value: (string | { nameInSource: string; nameInDestination: string })[],
+): Sql =>
+  raw(
+    value
+      .map((v) =>
+        typeof v === "string"
+          ? quoteIdentifier(v).sql
+          : `${quoteIdentifier(v.nameInSource).sql} AS ${
+              quoteIdentifier(v.nameInDestination).sql
+            }`,
+      )
+      .join(","),
+  );
