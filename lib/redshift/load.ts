833a88abcbbec22fbf5a08e021ba1598c74a98d5
Timothy Goltser <tim@realizefi.com>
2022-08-31 17:34:45 -0400

    Make compiler more pedantic about unchecked index access, add missing source types

---
+++ lib/redshift/load.ts
@@ -0,0 +1,170 @@
+import path from "path";
+import { Gzip } from "zlib";
+
+import { env } from "../env.js";
+import { uploadObject } from "../aws/upload.js";
+import { getColumnTypeForDest } from "../transform/index.js";
+import { ConnectionQueryOp, ConnectionQueryUnsafeOp } from "../connections.js";
+import { LoadDestination, Loader, LoadingActions } from "../load/index.js";
+
+class RedshiftLoader extends Loader implements LoadingActions {
+  #queryUnsafe: ConnectionQueryUnsafeOp;
+
+  constructor(
+    query: ConnectionQueryOp,
+    destination: LoadDestination,
+    queryUnsafe: ConnectionQueryUnsafeOp,
+  ) {
+    super(query, destination);
+    this.#queryUnsafe = queryUnsafe;
+  }
+
+  public createTable = async ({
+    schema,
+  }: {
+    schema: string;
+    database: string;
+  }) => {
+    const schemaCreateOperation = this.qb
+      .raw("create schema if not exists ??", [schema])
+      .toSQL()
+      .toNative();
+
+    await this.query(schemaCreateOperation);
+
+    const { configuration } = this.destination;
+
+    const columnsWithType = configuration.columns.map((destCol) => {
+      const columnType = destCol.viewColumn.dataType;
+
+      // todo(ianedwards): change this to use additional source and destination types
+      return `?? ${
+        getColumnTypeForDest({
+          sourceType: "POSTGRES",
+          destinationType: "REDSHIFT",
+          columnType,
+        }) ?? "varchar"
+      }`;
+    });
+
+    const tableCreateOperation = this.qb
+      .raw(`create table if not exists ?? ( ${columnsWithType.join(", ")} )`, [
+        this.tableName,
+        ...configuration.columns.map((col) => col.nameInDestination),
+      ])
+      .toSQL()
+      .toNative();
+
+    await this.query(tableCreateOperation);
+  };
+
+  stage = async (contents: Gzip) => {
+    const createStageOperation = this.qb
+      .raw(`create temp table if not exists ?? (like ??);`, [
+        this.stageName,
+        this.tableName,
+      ])
+      .toSQL()
+      .toNative();
+
+    await this.query(createStageOperation);
+
+    const pathPrefix = `redshift/${this.destination.id}`;
+    const { key } = await uploadObject({
+      contents,
+      pathPrefix,
+      extension: "gz",
+    });
+
+    const filePath = path.posix.join(
+      env.PROVISIONED_BUCKET_NAME,
+      pathPrefix,
+      `${key}.gz`,
+    );
+
+    const loadStageOperation = this.qb
+      .raw(
+        `
+        copy ?? from ?
+        credentials ? csv gzip timeformat as 'epochmillisecs' IGNOREHEADER 1
+        `,
+        [
+          this.stageName,
+          `s3://${filePath}`,
+          `aws_access_key_id=${env.S3_USER_ACCESS_ID};aws_secret_access_key=${env.S3_USER_SECRET_KEY}`,
+        ],
+      )
+      .toString();
+
+    await this.#queryUnsafe(loadStageOperation);
+  };
+
+  upsert = async () => {
+    const { configuration } = this.destination;
+    const { tableName, stageName } = this;
+
+    const names = configuration.columns.map((col) => col.nameInDestination);
+    const primaryKeyCol = configuration.view.columns.find(
+      (col) => col.isPrimaryKey,
+    );
+
+    if (!primaryKeyCol) {
+      throw new Error(
+        `View used by configuration ID = ${this.destination.configurationId} does not have a primary key col`,
+      );
+    }
+
+    const namesWithoutKey = names.filter((n) => n !== primaryKeyCol.name);
+
+    const updateCommand = this.qb
+      .raw(
+        `update ?? set
+        ${"?? = newData.??, ".repeat(namesWithoutKey.length).slice(0, -2)}
+        from ?? as newData
+        where ?? = newData.??
+      `,
+        [
+          tableName,
+          ...namesWithoutKey.flatMap((n) => [n, n]),
+          stageName,
+          `${tableName}.${primaryKeyCol.name}`,
+          primaryKeyCol.name,
+        ],
+      )
+      .toSQL()
+      .toNative();
+
+    await this.query(updateCommand);
+
+    const removeCommand = this.qb
+      .raw(`delete from ?? using ?? where ?? = ??`, [
+        stageName,
+        tableName,
+        `${stageName}.${primaryKeyCol.name}`,
+        `${tableName}.${primaryKeyCol.name}`,
+      ])
+      .toSQL()
+      .toNative();
+
+    await this.query(removeCommand);
+
+    const insertCommand = this.qb
+      .raw("insert into ?? select * from ??", [tableName, stageName])
+      .toSQL()
+      .toNative();
+
+    await this.query(insertCommand);
+  };
+
+  tearDown = async () => {
+    // todo(ianedwards): ensure file is deleted from S3
+
+    const dropStageOperation = this.qb
+      .raw("drop table ??", [this.tableName])
+      .toSQL()
+      .toNative();
+    await this.query(dropStageOperation);
+  };
+}
+
+export default RedshiftLoader;
---
+++ lib/transform/index.ts
@@ -0,0 +1,189 @@
+import { z } from "zod";
+
+/*
+ * Table for determining conversions between types across DBs
+ * Usage example: dataTypeConversions[src.sourceType][dest.destinationType][srcColumnType]
+ */
+const dataTypeConversionSchema = z.object({
+  SNOWFLAKE: z.object({
+    POSTGRES: z.object({
+      smallint: z.literal("smallint"),
+      bigint: z.literal("bigint"),
+      integer: z.literal("integer"),
+      decimal: z.literal("decimal"),
+      real: z.literal("real"),
+      "double precision": z.literal("double precision"),
+      boolean: z.literal("boolean"),
+      varchar: z.literal("varchar"),
+      text: z.literal("text"),
+      binary: z.literal("bytea"),
+      timestamp: z.literal("timestamp"),
+      timestamptz: z.literal("timestamp with time zone"),
+      date: z.literal("date"),
+      time: z.literal("time"),
+      object: z.literal("jsonb"),
+    }),
+    REDSHIFT: z.object({
+      smallint: z.literal("smallint"),
+      bigint: z.literal("bigint"),
+      integer: z.literal("integer"),
+      decimal: z.literal("decimal"),
+      real: z.literal("real"),
+      "double precision": z.literal("double precision"),
+      boolean: z.literal("boolean"),
+      varchar: z.literal("varchar"),
+      text: z.literal("text"),
+      binary: z.literal("bytea"),
+      timestamp: z.literal("timestamp"),
+      timestamptz: z.literal("timestamp with time zone"),
+      date: z.literal("date"),
+      time: z.literal("time"),
+    }),
+  }),
+  POSTGRES: z.object({
+    SNOWFLAKE: z.object({
+      smallint: z.literal("smallint"),
+      bigint: z.literal("bigint"),
+      integer: z.literal("integer"),
+      decimal: z.literal("decimal"),
+      real: z.literal("real"),
+      "double precision": z.literal("double precision"),
+      boolean: z.literal("boolean"),
+      varchar: z.literal("varchar"),
+      text: z.literal("text"),
+      bytea: z.literal("binary"),
+      timestamp: z.literal("timestamp"),
+      "timestamp without time zone": z.literal("timestamp"),
+      "timestamp with time zone": z.literal("timestamptz"),
+      date: z.literal("date"),
+      time: z.literal("time"),
+      jsonb: z.literal("variant"),
+      "USER-DEFINED": z.literal("varchar"),
+    }),
+    REDSHIFT: z.object({
+      smallint: z.literal("smallint"),
+      bigint: z.literal("bigint"),
+      integer: z.literal("integer"),
+      decimal: z.literal("decimal"),
+      real: z.literal("real"),
+      "double precision": z.literal("double precision"),
+      boolean: z.literal("boolean"),
+      char: z.literal("char"),
+      text: "text",
+      varchar: z.literal("varchar"),
+      timestamp: z.literal("timestamp"),
+      "timestamp without time zone": z.literal("timestamp"),
+      "timestamp with time zone": z.literal("timestamptz"),
+      date: z.literal("date"),
+      time: z.literal("time"),
+    }),
+  }),
+});
+export const dataTypeConversions: Readonly<
+  z.infer<typeof dataTypeConversionSchema>
+> = Object.freeze({
+  SNOWFLAKE: {
+    POSTGRES: {
+      smallint: "smallint",
+      bigint: "bigint",
+      integer: "integer",
+      decimal: "decimal",
+      real: "real",
+      "double precision": "double precision",
+      boolean: "boolean",
+      varchar: "varchar",
+      text: "text",
+      binary: "bytea",
+      timestamp: "timestamp",
+      timestamptz: "timestamp with time zone",
+      date: "date",
+      time: "time",
+      object: "jsonb",
+    },
+    REDSHIFT: {
+      smallint: "smallint",
+      bigint: "bigint",
+      integer: "integer",
+      decimal: "decimal",
+      real: "real",
+      "double precision": "double precision",
+      boolean: "boolean",
+      varchar: "varchar",
+      text: "text",
+      binary: "bytea",
+      timestamp: "timestamp",
+      timestamptz: "timestamp with time zone",
+      date: "date",
+      time: "time",
+      object: "jsonb",
+    },
+  },
+  POSTGRES: {
+    SNOWFLAKE: {
+      smallint: "smallint",
+      bigint: "bigint",
+      integer: "integer",
+      decimal: "decimal",
+      real: "real",
+      "double precision": "double precision",
+      boolean: "boolean",
+      varchar: "varchar",
+      text: "text",
+      bytea: "binary",
+      timestamp: "timestamp",
+      "timestamp without time zone": "timestamp",
+      "timestamp with time zone": "timestamptz",
+      date: "date",
+      time: "time",
+      jsonb: "variant",
+      "USER-DEFINED": "varchar",
+    },
+    REDSHIFT: {
+      smallint: "smallint",
+      bigint: "bigint",
+      integer: "integer",
+      decimal: "decimal",
+      real: "real",
+      "double precision": "double precision",
+      boolean: "boolean",
+      char: "char",
+      varchar: "varchar",
+      text: "text",
+      timestamp: "timestamp",
+      "timestamp without time zone": "timestamp",
+      "timestamp with time zone": "timestamptz",
+      date: "date",
+      time: "time",
+    },
+  },
+});
+
+export const getColumnTypeForDest = ({
+  sourceType,
+  destinationType,
+  columnType,
+}: {
+  sourceType: "POSTGRES";
+  destinationType: "SNOWFLAKE" | "REDSHIFT";
+  columnType: string;
+}) => {
+  if (!Object.hasOwn(dataTypeConversions, sourceType)) {
+    return null;
+  }
+
+  const conversionsBySrc = dataTypeConversions[sourceType];
+  if (!Object.hasOwn(conversionsBySrc, destinationType)) {
+    return null;
+  }
+
+  const srcConversionToDest = conversionsBySrc[destinationType];
+  if (!Object.hasOwn(srcConversionToDest, columnType)) {
+    return null;
+  }
+
+  return srcConversionToDest[
+    dataTypeConversionSchema.shape[sourceType].shape[destinationType]
+      .keyof()
+      .parse(columnType)
+  ];
+};
