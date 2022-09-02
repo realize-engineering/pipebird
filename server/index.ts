f97b40d655f7e758bd4a02af0bf1ebd39e87db9f
Curtis Mason <curtis@realizefi.com>
2022-09-02 13:23:30 -0400

    feat(graceful-shutdown): signal ctrl plane on shutdown

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app.js";
 import { env } from "../lib/env.js";
 import { logger } from "../lib/logger.js";
 import { transferQueue } from "./queues/transfer/scheduler.js";
+import got from "got";
 
 http.createServer(app).listen(env.PORT, () => {
   logger.info(`Server listening on :${env.PORT}`);
@@ -20,4 +21,11 @@ process.on("unhandledRejection", (reason) => {
 process.on("SIGTERM", async (reason) => {
   logger.warn(reason);
   await transferQueue.close();
+  try {
+    await got.delete("https://my.pipebird.com/api/deployment", {
+      headers: { Authorization: `Bearer ${env.LICENSE_KEY}` },
+    });
+  } catch (e) {
+    logger.error(e);
+  }
 });
