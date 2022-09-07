ea6d52b56384ce9fe09c97b40d57e7a5d9fa3ad3
Curtis Mason <31265687+cumason123@users.noreply.github.com>
2022-09-07 10:54:13 -0400

    Cumason/graceful shutdown (#76)
    * feat(graceful-shutdown): signal ctrl plane on shutdown
    
    * chore(version): bump 0.2.0 -> 0.2.1
    
    * feat(env): saving public key to .env on register script
    
    * feat(shutdown): added public key as header to graceful shutdown
    
    * fix(shutdown): .env no longer dockerignored
    
    * fix: localhost -> my.pipebird.com
    
    * fix: localhost -> my.pipebird.com in logs
    
    * feat(shutdown): fixed docker sigterm bug
    
    * refactor: renamed migrate.sh -> start.sh
    
    * refactor: deployment version 0.2.1 -> 0.2.2
    
    * refactor: fixed lingering ngrok url
    
    * refactor: renamed header x-pipebird-public-key -> x-pipebird-monitor-secret-key
    
    * refactor: renamed PUBLIC_KEY -> PIPEBIRD_MONITOR_SECRET_KEY and using patch on deployment
    
    * refactor: deployment using patch endpoint now
    
    * refactor: restored CMD entry in dockerfile
    
    * fix: pipebird notifs are async now
    
    * fix: process exit on sigterm
    
    Co-authored-by: Curtis Mason <curtis@realizefi.com>

--- server/index.ts
+++ server/index.ts
@@ -3,21 +3,60 @@ import { app } from "./app.js";
 import { env } from "../lib/env.js";
 import { logger } from "../lib/logger.js";
 import { transferQueue } from "./queues/transfer/scheduler.js";
+import got from "got";
 
-http.createServer(app).listen(env.PORT, () => {
+const gracefulShutdown = async () => {
+  await transferQueue.close();
+  try {
+    logger.info("Graceful shutdown pending");
+    await got.patch("https://my.pipebird.com/api/deployment", {
+      headers: {
+        "x-pipebird-monitor-secret-key":
+          process.env.PIPEBIRD_MONITOR_SECRET_KEY || "",
+      },
+      json: {
+        state: "GRACEFUL_SHUTDOWN_COMPLETED",
+      },
+    });
+    logger.info("Graceful shutdown complete");
+  } catch (e) {
+    logger.error(e);
+  }
+};
+
+http.createServer(app).listen(env.PORT, async () => {
   logger.info(`Server listening on :${env.PORT}`);
+  try {
+    await got.patch("https://my.pipebird.com/api/deployment", {
+      headers: {
+        "x-pipebird-monitor-secret-key":
+          process.env.PIPEBIRD_MONITOR_SECRET_KEY || "",
+      },
+      json: { state: "RUNNING" },
+    });
+    logger.info("Notified Pipebird monitor that instance is running.");
+  } catch (e) {
+    logger.warn({
+      monitorError: e,
+      message:
+        "Failed to notify Pipebird monitor that instance started running.",
+    });
+  }
 });
 
-process.on("uncaughtException", (error) => {
+process.on("uncaughtException", async (error) => {
   logger.fatal(error);
+  await gracefulShutdown();
   process.exit(1);
 });
 
-process.on("unhandledRejection", (reason) => {
+process.on("unhandledRejection", async (reason) => {
+  await gracefulShutdown();
   throw reason;
 });
 
 process.on("SIGTERM", async (reason) => {
   logger.warn(reason);
-  await transferQueue.close();
+  await gracefulShutdown();
+  process.exit(0);
 });
--- start.sh
+++ start.sh
@@ -4,8 +4,12 @@ if [ ! -f .env ]
 then
     # Prisma expects a local .env file
     echo "DATABASE_URL=$(printenv DATABASE_URL)" > .env
+    # TODO(cumason) refactor register_license.py -> register_license.ts and run as dockerfile build step
+    # PIPEBIRD_MONITOR_SECRET_KEY is set by register_license.py
+    echo "PIPEBIRD_MONITOR_SECRET_KEY=$(printenv PIPEBIRD_MONITOR_SECRET_KEY)" >> .env 
+
 fi
 
 npx prisma migrate deploy
 npx prisma db push
-npm start
+NODE_ENV="production" exec node dist/server/index.js
